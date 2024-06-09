737228467e2671a0a04a5c11c3bf7eb79767836c
Satyam Jaysawal <108862706+satyamjaysawal@users.noreply.github.com>
2024-06-09 12:09:38 +0530

    Update app.py

--- Image_Upload_Task/app.py
+++ Image_Upload_Task/app.py
@@ -39,7 +39,7 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
+------------------------------------------------------------
 
 # from flask import Flask, render_template, request
 
@@ -63,3 +63,43 @@ if __name__ == '__main__':
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
+
+-------------------------------------------------------------
+
+# from flask import Flask, request, redirect
+# from werkzeug.utils import secure_filename
+# import os
+
+# app = Flask(__name__)
+# app.config['UPLOAD_FOLDER'] = 'uploads'
+# app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
+# def allowed_file(filename):
+#     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+# @app.route('/', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         file = request.files['file']
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             return f'File {filename} uploaded successfully'
+#     return '''
+#     <!doctype html>
+#     <title>Upload an Image</title>
+#     <h1>Upload an Image</h1>
+#     <form method=post enctype=multipart/form-data>
+#       <input type=file name=file>
+#       <input type=submit value=Upload>
+#     </form>
+#     '''
+
+# if __name__ == "__main__":
+#     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+#     app.run(debug=True)
+
+------------------------------------------------------------
+
+
+
