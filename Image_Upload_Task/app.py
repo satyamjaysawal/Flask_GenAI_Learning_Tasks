f09326c41301ac200da35c5adeda89528bbf0fdc
Satyam Jaysawal <108862706+satyamjaysawal@users.noreply.github.com>
2024-06-09 11:52:03 +0530

    Update app.py

--- Image_Upload_Task/app.py
+++ Image_Upload_Task/app.py
@@ -41,22 +41,25 @@ if __name__ == '__main__':
 
 
 
-# from flask import Flask, request
+# from flask import Flask, render_template, request
 
 # app = Flask(__name__)
 
-# @app.route('/upload', methods=['POST'])
+# @app.route('/', methods=['GET', 'POST'])
 # def upload_file():
-#     if 'file' not in request.files:
-#         return 'No file part'
+#     if request.method == 'POST':
+#         if 'file' not in request.files:
+#             return 'No file part'
+        
+#         file = request.files['file']
+#         if file.filename == '':
+#             return 'No selected file'
+        
+#         if file:
+#             file.save(f"uploads/{file.filename}")
+#             return 'File successfully uploaded'
     
-#     file = request.files['file']
-#     if file.filename == '':
-#         return 'No selected file'
-    
-#     if file:
-#         file.save(f"/path/to/upload/directory/{file.filename}")
-#         return 'File successfully uploaded'
+#     return render_template('index.html')
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
